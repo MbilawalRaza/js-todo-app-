ab058fd7702c52aa7cfc3ea74457340697ddf70b
MbilawalRaza <155060676+MbilawalRaza@users.noreply.github.com>
2024-11-05 10:41:12 -0800

    todo app
    update version todo app

--- app.js
+++ app.js
@@ -1,69 +1,84 @@
 
-
-var input = document.getElementById("inp")
-var parent = document.getElementById('parent')
+var input = document.getElementById("inp");
+var parent = document.getElementById('parent');
 var data = [];
-let  oldVal='';
-var currentIndex = null 
-function addTodo(){
-  data.push({userval:input.value})
-  renderList()
-  input.value=''
+let oldVal = '';
+var currentIndex = null;
+
+function addTodo() {
+  if (!input.value) {
+    showAlert("Required field is missing");
+    return;
+  }
+  var currentDate = new Date();
+
+  var dateTimeString = `${currentDate.getFullYear()}-${String(currentDate.getMonth() + 1).padStart(2, '0')}-${String(currentDate.getDate()).padStart(2, '0')} ${String(currentDate.getHours()).padStart(2, '0')}:${String(currentDate.getMinutes()).padStart(2, '0')}`;
+  data.push({ userval: input.value, date: dateTimeString });
+  renderList();
+  console.log(data);
+  input.value = '';
 }
 
-
-function renderList(){
-
+function renderList() {
   parent.innerHTML = `
-  <ul>
-  ${data.map ( (item,index) =>`
-    
-    <li style=" list-style: none;">
-    <span> ${item.userval}</span>
-    <div  class="d-flex gap-4">
-    <button class="btn btn-info" onclick="editItem (${index}),getindex(${index})">edit todo</button>
-    <button class="btn btn-danger" onclick="deleteitem(${index})"> delete todo </button>
-    
-</div>
-</br>
-    
-    
-    </li>
+    <ul>
+    ${data.map((item, index) => `
+        <li style="list-style: none;" >
+        </br>
+            <span id="todotxt">${item.userval}</span>
+            <li id="dateli">date:<span>${item.date}</span></li>
+            <div class="d-flex gap-4">
+                <button class="btn btn-info" onclick="editItem(${index}), getindex(${index})">Edit Todo</button>
+                <button class="btn btn-danger" onclick="deleteitem(${index})">Delete Todo</button>
+            </div>
+        </li>
     `).join('')}
-  </ul>
-  `
+    </ul>
+    `;
 }
 
-function deleteitem(index){
-  data.splice(index,1)
-  renderList()
-}
-function deleteAll(){
-  data.splice(0,data.length)
-  renderList()
- }
-
-function getindex(index){
-  var oldValgetFun = data[index].userval;
-  oldVal = oldValgetFun;
-  currentIndex= index
+function deleteitem(index) {
+  data.splice(index, 1);
+  renderList();
 }
 
-function  editItem(index) {
-  todoadd.style.display = 'none'
-  todoupdate.style.display = 'block'
+function deleteAll() {
+  data.splice(0, data.length);
+  renderList();
+  todoadd.style.display = 'block';
+  todoupdate.style.display = 'none';
+}
+
+function getindex(index) {
+  oldVal = data[index].userval;
+  currentIndex = index;
+}
+
+function editItem(index) {
+  todoadd.style.display = 'none';
+  todoupdate.style.display = 'block';
   input.value = oldVal;
-  input.value=data[index].userval
+  input.value = data[index].userval;
 }
 
 function updatedvalreal() {
   const newval = input.value;
-  if (newval && currentIndex !== null) {  
-      data[currentIndex].userval = newval; 
-      renderList();
-      input.value = '';
-      todoadd.style.display = 'block'; 
-      todoupdate.style.display = 'none';  
-      currentIndex = null; 
+  if (newval && currentIndex !== null) {
+    data[currentIndex].userval = newval;
+    renderList();
+    input.value = '';
+    todoadd.style.display = 'block';
+    todoupdate.style.display = 'none';
+    currentIndex = null;
   }
 }
+
+function showAlert(message) {
+  document.getElementById("alertMessage").innerText = message;
+  document.getElementById("customAlert").style.display = "flex";
+}
+
+function closeAlert() {
+  document.getElementById("customAlert").style.display = "none";
+}
+
